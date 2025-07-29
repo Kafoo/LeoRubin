8fc7280956e15178b002c41a987be10527825976
Kafoo <ant.guillard@gmail.com>
2025-07-29 11:19:49 +0200

    style: Refine project section styling and content

--- components/Creation/ProjectSection.vue
+++ components/Creation/ProjectSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <ContentCard class="mx-3 mb-10" style="margin-top: -150px;">
+  <ContentCard class="mx-3">
     <v-row class="pa-2">
       <v-col
         v-for="(item, i) in projectItems"
@@ -9,40 +9,41 @@
         lg="4"
         class="text-center pa-5"
       >
-        <v-icon :icon="item.icon" size="64" class="mb-4" color="grey-darken-1"></v-icon>
+        <v-icon :icon="item.icon" size="64" class="mb-4" color="#574836"></v-icon>
         <h3 class="text-h6 mb-2 project-title">{{ item.title }}</h3>
-        <p class="text-body-1 project-subtitle">{{ item.subtitle }}</p>
+        <div class="text-body-1 project-subtitle">{{ item.subtitle }}</div>
       </v-col>
     </v-row>
     <template #footer>
       <div class="text-center centering-column">
         <div class="footer-text my-6">
-          <p class="mb-3" style="font-size: 1.6rem; font-family: 'Recursive';">
+          <div class="mb-3 footer-catchphrase">
             Quel que soit votre projet
-          </p>
-          <p>
-            N'hésitez pas à m'appeler ou m'écrire, un premier contact n'engage à rien et permettra de savoir si nous avons envie de nous lancer ensemble dans l'aventure !
-          </p>
+          </div>
+          <div>
+            N'hésitez pas à m'appeler ou m'écrire ! <br>
+            Un premier contact n'engage à rien et permet de savoir si nous pouvons jouer sur la même partition.
+          </div>
         </div>
         <div class="d-flex justify-center ga-4 mt-2 mb-3">
           <v-tooltip text="ant.guillard@gmail.com" location="top">
             <template #activator="{ props }">
               <v-btn v-bind="props" icon href="mailto:ant.guillard@gmail.com" size="large" color="white" elevation="2">
-                <v-icon color="#0d490d">mdi-email-outline</v-icon>
+                <v-icon>mdi-email-outline</v-icon>
               </v-btn>
             </template>
           </v-tooltip>
           <v-tooltip text="06 42 40 29 16" location="top">
             <template #activator="{ props }">
               <v-btn v-bind="props" icon href="tel:+33642402916" size="large" color="white" elevation="2">
-                <v-icon color="#0d490d">mdi-phone-outline</v-icon>
+                <v-icon>mdi-phone-outline</v-icon>
               </v-btn>
             </template>
           </v-tooltip>
           <v-tooltip text="M'écrire" location="top">
             <template #activator="{ props }">
               <v-btn v-bind="props" icon href="/contact" size="large" color="white" elevation="2">
-                <v-icon color="#0d490d">mdi-send-outline</v-icon>
+                <v-icon>mdi-send-outline</v-icon>
               </v-btn>
             </template>
           </v-tooltip>
@@ -73,7 +74,7 @@ const projectItems = [
   },
   {
     icon: 'mdi-tools',
-    title: 'Développer un outil sur-mesure',
+    title: 'Développer sur-mesure',
     subtitle: 'Créer une application web ou un intranet pour répondre à un besoin métier spécifique.',
   },
   {
@@ -92,14 +93,24 @@ const projectItems = [
 <style scoped>
 
 .project-title {
-  color: #0d490d;
+  font-family: 'Recursive', sans-serif;
+  letter-spacing : -0.5px !important;
+}
+
+.project-subtitle{
+  color: rgb(77, 77, 77);
 }
 
 .footer-text{
   font-family: 'Montserrat', sans-serif;
-  letter-spacing: 1px;
-  font-size: 1.2rem;
+  letter-spacing: 0.5px;
+  font-size: 1.1rem;
   max-width: 750px;
+}
+
+.footer-catchphrase{
+  font-family: 'Recursive', sans-serif;
+  font-size: 1.9rem;
 }
 
 </style>
